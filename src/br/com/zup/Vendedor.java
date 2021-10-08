389bd40d839f90d631d5382a9c32b9d402470158
Debora Souza <deboral.p@hotmail.com>
2021-10-08 20:06:06 -0300

    Modelar classe Vendedor

--- src/br/com/zup/Vendedor.java
+++ src/br/com/zup/Vendedor.java
@@ -1,5 +1,8 @@
 package br.com.zup;
 
-public class Vendedor {
+public class Vendedor extends Pessoa{
+    public Vendedor(String nome, String email, String cpf) {
+        super(nome, email, cpf);
+    }
 
 }
