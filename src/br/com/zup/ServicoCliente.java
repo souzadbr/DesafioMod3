c9665b8b9332d6aa76c157a38b14282f4979a49d
Debora Souza <deboral.p@hotmail.com>
2021-10-08 20:27:33 -0300

    Cadastrar 1 cliente

---
+++ src/br/com/zup/ServicoCliente.java
@@ -0,0 +1,24 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicoCliente {
+
+    private static List<Cliente>clientes = new ArrayList<>();
+
+    //Método Cadastrar cliente
+    public static Cliente cadastrarCliente(String nome, String email, String cpf) throws Exception {
+
+        Cliente cliente = new Cliente(nome, email, cpf);
+
+        clientes.add(cliente);
+        return cliente;
+    }
+
+
+    //Método que mostra clientees cadastrados
+    public static void exibirClientesCadastrados() {
+        System.out.println(clientes);
+    }
+}
--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,4 +1,32 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Sistema {
+    //Método que recebe dados
+    private static Scanner capturarDados(String mensagem) {
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
+
+    //Método que exibi as opções do usuario para serem implementadas
+    public static void menu() {
+        System.out.println("Digite 1 - para cadastrar cliente. ");
+        System.out.println("Digite 2 - para cadastrar Vendedor. ");
+        System.out.println("Digite 3 - para cadastrar Venda. ");
+        System.out.println("Digite 4 - para listar Vendas Cadastradas. ");
+        System.out.println("Digite 5 - para listar Vendedores Cadastrados. ");
+        System.out.println("Digite 6 - para listas Clientes Cadastrados. ");
+        System.out.println("digite 7 - para SAIR. ");
+    }
+
+    //Método Cadastrar cliente
+    public static Cliente cadastrarCliente() throws Exception {
+        String nome = capturarDados("Digite o nome do Cliente: ").nextLine();
+        String email = capturarDados("Digite o e-mail do cliente: ").nextLine();
+        String cpf = capturarDados("Digite o CPF do cliente:").nextLine();
+
+        return ServicoCliente.cadastrarCliente(nome, email, cpf);
+    }
+
 }
